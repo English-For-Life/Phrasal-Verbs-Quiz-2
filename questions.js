4fb43089b33a84950f4fc3a367b20f537f763a31
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-28 21:15:34 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+//Array of objects
+const quiz = [
+    {
+        q:"If you don't know any words, look them ___ in a dictionary.",
+        options:["up", "through", "out", "on"],
+        answer: 0
+    },
+    {
+     q:"Can you look ___ my cat when I'm away.",
+        options:["through", "after", "up", "for"],
+        answer: 1   
+    },
+    {
+        q:"I woke up earlier than usual to look ___ my notes.",
+        options:["out", "up", "through", "back"],
+        answer: 2
+    },
+    {
+        q:"Look ___ ! I've just mopped the floor and it's wet. ",
+        options:["back", "in", "on", "out"],
+        answer: 3
+    },
+    {
+        q:"They'll have to look ___ and see what options they have.",
+        options:["ahead", "out", "to", "after"],
+        answer: 0
+    },
+    {
+        q:"When we had a bump last week many passers by stopped to look ___ .",
+        options:["up", "on", "out", "for"],
+        answer: 1
+    },
+    {
+        q:"When my grandma looks ___ she can see how happy she was in her childhood. ",
+        options:["through", "down", "back", "ahead"],
+        answer: 2
+    },
+    {
+        q:"They looked ___ and smiled at me when I called them.",
+        options:["out", "forward", "ahead", "round"],
+        answer: 3
+    },
+    {
+        q:"My son is looking ___ leaving school. ",
+        options:["forward", "back", "after", "for"],
+        answer: 0
+    },
+    {
+        q:"Alex has finally found his keys he was looking ___ all morning.",
+        options:["in", "for", "forward", "out"],
+        answer: 1
+    },
+    {
+        q:"We looked ___ our friends for support but we were disappointed.",
+        options:["on", "out", "to", "up"],
+        answer: 2
+    },
+    {
+        q:"Yesterday afternoon I looked ___ on my grandpa on my way from work.",
+        options:["after", "ahead", "out", "in"],
+        answer: 3
+    },
+    {
+        q:"The audience looked ___ delighted as the actors performed.",
+        options:["on", "down", "up", "to"],
+        answer: 0
+    },
+    {
+        q:"I look ___ my neighbour's kids to earn some money.",
+        options:["at", "after", "for", "out"],
+        answer: 1
+    },
+    {
+        q:"Mark look ___ the magazine and put it back.",
+        options:["up", "out", "through", "to"],
+        answer: 2
+    },
+    {
+        q:"Our parents are looking ___ seeing us again this summer.",
+        options:["on", "round", "to", "forward"],
+        answer: 3
+    },
+    {
+        q:"If I were you I wouldn't look ___ your boyfriend for help.",
+        options:["to", "after", "out", "in"],
+        answer: 0
+    },
+    {
+        q:"I am looking ___ my best friend.",
+        options:["ahead", "forward", "up", "through"],
+        answer: 1
+    },
+    {
+        q:"We just looked ___ to say hello.",
+        options:["on", "up", "in", "out"],
+        answer: 2
+    },
+    {
+        q:"If you don't look ___ , you'll get fired soon.",
+        options:["up", "through", "after", "out"],
+        answer: 3
+    }
+]
